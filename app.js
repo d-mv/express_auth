5fcef343967d540485df96cab01f36e2cfad036b
d-mv <d.dash.mv@gmail.com>
2019-03-20 23:36:31 +0200

    Views & routes done

--- app.js
+++ app.js
@@ -1,33 +1,86 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sassMiddleware = require("node-sass-middleware");
+const session = require("express-session");
+const passport = require("passport");
+const expressValidator = require('express-validator')
+const LocalStrategy = require("passport-local").Strategy;
+const multer = require("multer");
+const upload = multer({dest: './uploads'})
+const flash = require("connect-flash");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const db = mongoose.connection;
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+// handle file uploads
+// app.use(multer({ dest: "./uploads" }));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+// handle sessions
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// handle passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// handle validations
+app.use(
+  expressValidator({
+    errorFormatter: (param, msg, value) => {
+      const namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += `[${namespace.shift()}]`;
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+// handle messages
+app.use(require("connect-flash")());
+app.use((req, res, next) => {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,11 +91,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "./uploads" });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", (req, res, next) => {
+  res.send("respond with a resource");
+});
+router.get("/register", (req, res, next) => {
+  res.render("register", { title: "Register" });
+});
+router.get("/login", (req, res, next) => {
+  res.render("login", { title: "Login" });
+});
+router.post("/register", upload.single("profileimage"), (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+  if (req.file) {
+    console.log("-> Uploading file...");
+    const profileimage = req.file.filename;
+  } else {
+    console.log("-> No file uploaded...");
+    const profileimage = "noimage.jpg";
+  }
+  // Form validation
+  req.checkBody("name", "Name field is required").notEmpty();
+  req.checkBody("email", "Email field is required").notEmpty();
+  req.checkBody("email", "Email is not valid").isEmail();
+  req.checkBody("username", "Username field is required").notEmpty();
+  req.checkBody("password", "Password field is required").notEmpty();
+  req
+    .checkBody("password2", "Passwords do not match")
+    .equals(req.body.password);
+
+  // check for errors
+  const errors = req.validationErrors();
+
+  if (errors) {
+    res.render('register', {errors: errors})
+    console.log("Errors");
+  } else {
+    console.log("No errors");
+  }
 });
 
 module.exports = router;
